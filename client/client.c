0b686b41235b54525be492eb9e293fb1fdb36092
Collin <collin.nelson@wsu.edu>
2022-02-15 17:34:49 -0800

    Tab aligned the code, and reimplemented multithreaded server processes

--- client/client.c
+++ client/client.c
@@ -17,7 +17,7 @@
 #include <time.h> 
 
 #define MAX 256
-#define PORT 1234
+#define PORT 2222
 
 char line[MAX], ans[MAX];
 int n;
@@ -27,45 +27,45 @@ int sfd;
 
 int main(int argc, char *argv[], char *env[]) 
 { 
-    int n; char how[64];
-    int i;
+  int n; char how[64];
+  int i;
 
-    printf("1. create a socket\n");
-    sfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sfd < 0) { 
-        printf("socket creation failed\n"); 
-        exit(0); 
-    }
+  printf("1. create a socket\n");
+  sfd = socket(AF_INET, SOCK_STREAM, 0); 
+  if (sfd < 0) { 
+    printf("socket creation failed\n"); 
+      exit(0); 
+  }
     
-    printf("2. fill in server IP and port number\n");
-    bzero(&saddr, sizeof(saddr)); 
-    saddr.sin_family = AF_INET; 
-    saddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    saddr.sin_port = htons(PORT); 
+  printf("2. fill in server IP and port number\n");
+  bzero(&saddr, sizeof(saddr)); 
+  saddr.sin_family = AF_INET; 
+  saddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+  saddr.sin_port = htons(PORT); 
   
-    printf("3. connect to server\n");
-    if (connect(sfd, (struct sockaddr *)&saddr, sizeof(saddr)) != 0) { 
-        printf("connection with the server failed...\n"); 
-        exit(0); 
-    } 
+  printf("3. connect to server\n");
+  if (connect(sfd, (struct sockaddr *)&saddr, sizeof(saddr)) != 0) { 
+      printf("connection with the server failed...\n"); 
+      exit(0); 
+  } 
 
-    printf("********  processing loop  *********\n");
-    while (1){
-      printf("input a line : ");
-      bzero(line, MAX);                // zero out line[ ]
-      fgets(line, MAX, stdin);         // get a line (end with \n) from stdin
+  printf("********  processing loop  *********\n");
+  while (1){
+    printf("input a line : ");
+    bzero(line, MAX);                // zero out line[ ]
+    fgets(line, MAX, stdin);         // get a line (end with \n) from stdin
 
-      line[strlen(line)-1] = 0;        // kill \n at end
-      if (line[0]==0)                  // exit if NULL line
-         exit(0);
+    line[strlen(line)-1] = 0;        // kill \n at end
+    if (line[0]==0)                  // exit if NULL line
+      exit(0);
 
-      // Send ENTIRE line to server
-      n = write(sfd, line, MAX);
-      printf("client: wrote n=%d bytes; line=(%s)\n", n, line);
+    // Send ENTIRE line to server
+    n = write(sfd, line, MAX);
+    printf("client: wrote n=%d bytes; line=(%s)\n", n, line);
 
-      // Read a line from sock and show it
-      n = read(sfd, ans, MAX);
-      printf("client: read  n=%d bytes; echo=(%s)\n",n, ans);
+    // Read a line from sock and show it
+    n = read(sfd, ans, MAX);
+    printf("client: read  n=%d bytes; echo=(%s)\n",n, ans);
   }
 }
 
